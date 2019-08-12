ad92e652f49976c53ad0c514118a98c7b4d31624
gabby1995-tech <54010569+gabby1995-tech@users.noreply.github.com>
2019-08-11 20:40:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var currentSaying = '';
   '*Two annoying white girls talking to each other* Girl 1: "OMG are you excited that we\'re rooming together at college this fall?" Girl 2: "Totally, what should we do when we first check into the dorms?" Gabs: "I don\'t know, Les out?"',
   '"While I am a virgin, I\'m ready for the big leagues when I get down. I\'ve probably eclipsed a 1000 hours of watching porn, which are basically training videos"' ,
   '"Does Alton have black people?"' , '*Talking to friends from Alton* Gabs: "Do you guys have indoor plumbing in Alton?"', '"I equate jerking off to a drug addiction, I\'m just trying to chase the dragon of the first ejaculation I had when I was 13"',
-  '"Have you ever eaten a bratwurst while laying on your bed?"', '"Would you guys ever pee on a girl if she told you to?"', '""'];
+  '"Have you ever eaten a bratwurst while laying on your bed?"', '"Would you guys ever pee on a girl if she told you to?"', '"You\'re as useless as a old civil war air conditioner"', '"Why don\'t we shower as a team?"', '"Can I be called Miroslav from now on instead of Gabs?"',];
 
 
 function newQuote() {
